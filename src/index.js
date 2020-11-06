7fedf98e8b9a008270d134640223d50de663849c
kevinfiol <me@kevinfiol.com>
2020-11-05 20:18:01 -0500

    Meiosis pattern using useReducer

--- src/index.js
+++ src/index.js
@@ -1,29 +1,41 @@
 import './styles/index.css';
 
 import { h, render } from 'preact';
-import { useState } from 'preact/hooks';
-
+import { useReducer, useRef } from 'preact/hooks';
 import Counter from './components/Counter';
 
-const App = () => {
-    const [num, setNum] = useState(0);
-    
-    function increment() {
-        setNum(num + 1);
-    }
+const State = () => ({ count: 0 });
 
-    function decrement() {
-        setNum(num - 1);
-    }
+const Actions = dispatch => ({
+    increment: () => dispatch(state => {
+        return { ...state, count: state.count + 1 };
+    }),
+
+    decrement: () => dispatch(state => {
+        return { ...state, count: state.count - 1 };
+    })
+});
+
+const useGlobalState = (createActions, initialState) => {
+    const [state, dispatch] = useReducer((x, f) => f(x), initialState);
+    const actionsRef = useRef();
+
+    if (!actionsRef.current)
+        actionsRef.current = createActions(dispatch);
+    return [state, actionsRef.current];
+};
+
+const App = () => {
+    const [state, actions] = useGlobalState(Actions, State());
 
     return (
         <div className="max-width-3 mx-auto p3">
             <h1>Preact Starter</h1>
-            <p class="who-cares">This is a sample Preact application.</p>
+            <p>This is a sample Preact application.</p>
             <Counter
-                num={num}
-                increment={increment}
-                decrement={decrement}
+                num={state.count}
+                increment={actions.increment}
+                decrement={actions.decrement}
             />
         </div>
     );
